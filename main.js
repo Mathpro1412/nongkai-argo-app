6ed8937a7dbde37a2b484ff7c52fa189739bad8e
Anon Jansaichon <a.jansaichon@gmail.com>
2025-05-13 23:23:11 +0700

    chore: fix bug

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-  res.send('Hello Helm from Super-Prod!')
+  res.send('Hello Helm from Prod!')
 })
 
 app.listen(port, () => {
