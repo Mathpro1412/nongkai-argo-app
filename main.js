f1fd1b438a63b501bf8d6da4cbf82214e453ad59
Anon Jansaichon <a.jansaichon@gmail.com>
2025-05-17 22:02:33 +0700

    chore: add env

--- main.js
+++ main.js
@@ -1,9 +1,11 @@
 const express = require('express')
+require('dotenv').config() 
+
 const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-  res.send('Hello Helm World!')
+  res.send(`Hello ${process.env.NAME} World!`)
 })
 
 app.listen(port, () => {
