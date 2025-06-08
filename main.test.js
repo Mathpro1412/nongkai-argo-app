9fbdb44fb306ca785a8f513ca982d67ab2cd7587
Anon Jansaichon <a.jansaichon@gmail.com>
2025-06-08 20:43:22 +0700

    chore: fix code

--- main.test.js
+++ main.test.js
@@ -17,6 +17,6 @@ describe('GET /', () => {
   it('should return Hello World!', async () => {
     const response = await request(app).get('/');
     expect(response.status).toBe(200);
-    expect(response.text).toBe('Hello World01!');
+    expect(response.text).toBe('Hello World!');
   });
 });
